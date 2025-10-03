c4fc4d177e078960d1eff3b48df1301eb3a64ac2
Kizzy <66209284+Ranisimo@users.noreply.github.com>
2025-10-03 15:29:14 +0100

    Update CVDownload.tsx

--- src/components/CVDownload.tsx
+++ src/components/CVDownload.tsx
@@ -1,7 +1,7 @@
 const CVDownload: React.FC = () => {
   return (
     <>
-      <a href="_next/static/media/KizzySinarResume.pdf" className="btn btn-outline-custom btn-rounded mt-4" download="KizzySinarCV.pdf">Download My CV</a >
+      <a href="https://9l7y52aysvzhdae9.public.blob.vercel-storage.com/public/SinarKizzyCV.pdf" className="btn btn-outline-custom btn-rounded mt-4" download="KizzySinarCV.pdf">Download My CV</a >
     </>
   );
 };
